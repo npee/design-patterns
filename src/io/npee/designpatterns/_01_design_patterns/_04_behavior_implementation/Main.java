806575e845e73e32d00db762b47b80f5e2f36aad
npee <jmc4664@gmail.com>
2022-02-06 16:52:53 +0900

    오리 타입 수정

--- src/io/npee/designpatterns/_01_design_patterns/_04_behavior_implementation/Main.java
+++ src/io/npee/designpatterns/_01_design_patterns/_04_behavior_implementation/Main.java
@@ -2,17 +2,17 @@ package io.npee.designpatterns._01_design_patterns._04_behavior_implementation;
 
 public class Main {
 	public static void main(String[] args) {
-		MallardDuck mallardDuck = new MallardDuck();
+		Duck mallardDuck = new MallardDuck();
 		mallardDuck.display();
 		mallardDuck.performFly();
 		mallardDuck.performQuack();
 
-		RedHeadDuck redheadDuck = new RedHeadDuck();
+		Duck redheadDuck = new RedHeadDuck();
 		redheadDuck.display();
 		redheadDuck.performFly();
 		redheadDuck.performQuack();
 
-		RubberDuck rubberDuck = new RubberDuck();
+		Duck rubberDuck = new RubberDuck();
 		rubberDuck.display();
 		rubberDuck.performFly();
 		rubberDuck.performQuack();
