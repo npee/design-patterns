5d2fe64c862541305eb48429ac3a8febfc1c0a0d
npee <jmc4664@gmail.com>
2022-02-09 22:09:33 +0900

    날씨 데이터 객체 기본 코드

---
+++ src/io/npee/designpatterns/_02_observer/_01_base/WeatherData.java
@@ -0,0 +1,27 @@
+package io.npee.designpatterns._02_observer._01_base;
+
+public class WeatherData {
+
+	private float temperature;
+	private float humidity;
+	private float pressure;
+
+	/**
+	 * 기상 관측값이 갱신될 떄 마다 알려주기 위한 메서드
+	 */
+	public void measurementsChanged() {
+		// to-do
+	}
+
+	public double getTemperature() {
+		return temperature;
+	}
+
+	public double getHumidity() {
+		return humidity;
+	}
+
+	public double getPressure() {
+		return pressure;
+	}
+}
