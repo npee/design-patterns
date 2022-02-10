4969a36f832475ba900a2211660fbce9165828a6
npee <jmc4664@gmail.com>
2022-02-10 22:30:23 +0900

    WeatherData 수정

--- src/io/npee/designpatterns/_02_observer/_02_weather_station/WeatherData.java
+++ src/io/npee/designpatterns/_02_observer/_02_weather_station/WeatherData.java
@@ -1,16 +1,51 @@
 package io.npee.designpatterns._02_observer._02_weather_station;
 
-public class WeatherData {
+import java.util.ArrayList;
+import java.util.List;
 
+public class WeatherData implements Subject {
+
+	private List<Observer> observers;
 	private float temperature;
 	private float humidity;
 	private float pressure;
+
+	public WeatherData() {
+		this.observers = new ArrayList<>();
+	}
+
+	@Override
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		int i = observers.indexOf(o);
+		if (i >= 0) {
+			observers.remove(o);
+		}
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Observer observer : observers) {
+			observer.update(temperature, humidity, pressure);
+		}
+	}
 
 	/**
 	 * 기상 관측값이 갱신될 떄 마다 알려주기 위한 메서드
 	 */
 	public void measurementsChanged() {
-		// to-do
+		notifyObservers();
+	}
+
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
 	}
 
 	public double getTemperature() {
