19b6ae3cde34f41b62096a92a6a8988473ab48bf
npee <jmc4664@gmail.com>
2022-02-10 20:51:56 +0900

    Updatable 구현체 작성

---
+++ src/io/npee/designpatterns/_02_observer/_02_rough_development/ForecastDisplay.java
@@ -0,0 +1,9 @@
+package io.npee.designpatterns._02_observer._02_rough_development;
+
+public class ForecastDisplay implements Updatable {
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		System.out.println("Show forecast.");
+	}
+}
