b459a9dc5242cd3796a91e355beea762a1126c87
npee <jmc4664@gmail.com>
2022-02-23 23:57:02 +0900

    SimpleFactory의 createPizza()를 PizzaStore의 추상 메소드로 변경

---
+++ src/io/npee/designpatterns/_04_factory/_03_factory/Pizza.java
@@ -0,0 +1,20 @@
+package io.npee.designpatterns._04_factory._03_factory;
+
+public abstract class Pizza {
+
+	public void prepare() {
+		System.out.println("사전 준비");
+	}
+
+	public void bake() {
+		System.out.println("굽기");
+	}
+
+	public void cut() {
+		System.out.println("자르기");
+	}
+
+	public void box() {
+		System.out.println("포장하기");
+	}
+}
---
+++ src/io/npee/designpatterns/_04_factory/_03_factory/PizzaStore.java
@@ -0,0 +1,18 @@
+package io.npee.designpatterns._04_factory._03_factory;
+
+public abstract class PizzaStore {
+
+	Pizza orderPizza(String type) {
+
+		Pizza pizza = createPizza(type);
+
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+
+		return pizza;
+	}
+
+	abstract Pizza createPizza(String type);
+}
