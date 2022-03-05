d346aea889b4b9c912c4699ad7e592423ddb7f97
npee <jmc4664@gmail.com>
2022-03-05 16:21:38 +0900

    초콜릿 공장 행동 추가

--- src/io/npee/designpatterns/_05_singleton/_02_chocolate_factory/ChocolateBoiler.java
+++ src/io/npee/designpatterns/_05_singleton/_02_chocolate_factory/ChocolateBoiler.java
@@ -26,20 +26,27 @@ public class ChocolateBoiler {
 			empty = false;
 			boiled = false;
 			// 보일러에 우유/초콜릿을 혼합한 재료를 집어넣음
+			System.out.println("Mix milk and chocolate");
+			System.out.println("Fill boiler");
+			System.out.println("====");
 		}
 	}
 
 	public void drain() {
 		if (!isEmpty() && isBoiled()) {
-			// 끓인 재료를 다음 단계로 넘김
 			empty = true;
+			// 끓인 재료를 다음 단계로 넘김
+			System.out.println("Pass the boiled over");
+			System.out.println("====");
 		}
 	}
 
 	public void boil() {
 		if (!isEmpty() && !isBoiled()) {
+			boiled = true;
 			// 재료를 끓임
-			boiled = false;
+			System.out.println("Boil");
+			System.out.println("====");
 		}
 	}
 
