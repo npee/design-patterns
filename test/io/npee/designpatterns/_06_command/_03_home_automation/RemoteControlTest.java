3dabfafe570aaeb7f3788aa95b994fce23141721
npee <jmc4664@gmail.com>
2022-03-13 17:49:03 +0900

    리모컨 테스트

---
+++ test/io/npee/designpatterns/_06_command/_03_home_automation/RemoteControlTest.java
@@ -0,0 +1,53 @@
+package io.npee.designpatterns._06_command._03_home_automation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RemoteControlTest {
+
+	@Test
+	void remoteControlTest() {
+		RemoteControl remoteControl = new RemoteControl();
+
+		Light livingRoomLight = new Light("거실");
+		Light kitchenLight = new Light("부엌");
+		CeilingFan ceilingFan= new CeilingFan("거실");
+		GarageDoor garageDoor = new GarageDoor("차고");
+		Stereo stereo = new Stereo("거실");
+
+		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+		LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+		LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
+
+		CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
+		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+
+		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
+
+		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+		StereoOffCommand  stereoOff = new StereoOffCommand(stereo);
+
+		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+		remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+		remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
+		remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+		remoteControl.setCommand(4, garageDoorUp, garageDoorDown);
+
+		System.out.println(remoteControl);
+
+		remoteControl.onButtonWasPushed(0);
+		remoteControl.offButtonWasPushed(0);
+		remoteControl.onButtonWasPushed(1);
+		remoteControl.offButtonWasPushed(1);
+		remoteControl.onButtonWasPushed(2);
+		remoteControl.offButtonWasPushed(2);
+		remoteControl.onButtonWasPushed(3);
+		remoteControl.offButtonWasPushed(3);
+		remoteControl.onButtonWasPushed(4);
+		remoteControl.offButtonWasPushed(4);
+	}
+
+}
