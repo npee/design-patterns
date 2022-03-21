a6ca966c53831f4e7266f150b47a95ef7ca1e2e5
npee <jmc4664@gmail.com>
2022-03-21 09:24:21 +0900

    JFrame 테스트

---
+++ src/io/npee/designpatterns/_09_templatemethod/_05_swing_frame/MyFrame.java
@@ -0,0 +1,25 @@
+package io.npee.designpatterns._09_templatemethod._05_swing_frame;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame(String title) {
+        super(title);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(300, 300);
+        this.setVisible(true);
+    }
+
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        String msg = "내가 최고!";
+        graphics.drawString(msg, 100, 100);
+    }
+
+    public static void main(String[] args) {
+        new MyFrame("헤드 퍼스트 디자인 패턴");
+    }
+}
