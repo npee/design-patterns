cba4c73a63d74dd08da33133099ca1153f6a446f
npee <jmc4664@gmail.com>
2022-03-28 09:28:40 +0900

    Cafe 메뉴 추가

---
+++ src/io/npee/designpatterns/_10_iterator/_04_add_cafe_menu/CafeMenu.java
@@ -0,0 +1,32 @@
+package io.npee.designpatterns._10_iterator._04_add_cafe_menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CafeMenu {
+
+    HashMap<String, MenuItem> menuItems = new HashMap<String, MenuItem>();
+
+    public CafeMenu() {
+        addItem("베지 버거와 에어 프라이",
+                "통밀 번, 양상추, 토마토, 감자튀김을 곁들인 베지 버거",
+                true, 3.99);
+        addItem("오늘의 스프",
+                "사이드 샐러드를 곁들인 오늘의 스프",
+                false, 3.69);
+        addItem("브리또",
+                "통 핀토빈과 살사, 과카몰이 곁들여진 거대한 브리또",
+                true, 4.29);
+    }
+
+    public void addItem(String name, String description,
+                        boolean vegetarian, double price)
+    {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.put(name, menuItem);
+    }
+
+    public Map<String, MenuItem> getItems() {
+        return menuItems;
+    }
+}
