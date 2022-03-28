ee805f323dbd0e6eb3b0cbc0a840b20f36e214a0
npee <jmc4664@gmail.com>
2022-03-28 20:30:20 +0900

    저녁메뉴가 추가된 웨이트리스 테스트

---
+++ test/io/npee/designpatterns/_10_iterator/_04_add_cafe_menu/WaitressTest.java
@@ -0,0 +1,21 @@
+package io.npee.designpatterns._10_iterator._04_add_cafe_menu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WaitressTest {
+
+    @Test
+    void waitressTest() {
+
+        Menu dinerMenu = new DinerMenu();
+        Menu pancakeHouseMenu = new PancakeHouseMenu();
+        Menu cafeMenu = new CafeMenu();
+
+        Waitress waitress = new Waitress(dinerMenu, pancakeHouseMenu, cafeMenu);
+
+        waitress.printMenu();
+    }
+
+}
