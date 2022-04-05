b1540d19f1be5a98a1951432634a0c24be3e3381
npee <jmc4664@gmail.com>
2022-04-05 22:13:43 +0900

    풍선껌 기계 테스트 02

---
+++ test/io/npee/designpatterns/_12_state/_02_state/GumballMachineTest.java
@@ -0,0 +1,32 @@
+package io.npee.designpatterns._12_state._02_state;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GumballMachineTest {
+
+    @Test
+    void gumballMachineTest() {
+        GumballMachine gumballMachine = new GumballMachine(2);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.refill(5);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+
+}
