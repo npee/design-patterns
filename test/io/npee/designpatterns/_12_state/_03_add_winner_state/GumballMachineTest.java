d797b9f087573e713fc855657d587135468ba4d2
npee <jmc4664@gmail.com>
2022-04-07 09:01:39 +0900

    풍선껌 리필 테스트

--- test/io/npee/designpatterns/_12_state/_03_add_winner_state/GumballMachineTest.java
+++ test/io/npee/designpatterns/_12_state/_03_add_winner_state/GumballMachineTest.java
@@ -7,6 +7,27 @@ import org.junit.jupiter.api.Test;
 class GumballMachineTest {
 
     @Test
+    void gumballMachineSoldOutTest() {
+        GumballMachine gumballMachine =
+            new GumballMachine(10);
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(gumballMachine);
+
+            gumballMachine.insertQuarter();
+            gumballMachine.turnCrank();
+            gumballMachine.insertQuarter();
+            gumballMachine.turnCrank();
+
+            System.out.println(gumballMachine);
+
+            if (gumballMachine.getCount() < 5) {
+                gumballMachine.refill(gumballMachine.getCount());
+            }
+        }
+    }
+
+    @Test
     void gumballMachineTest() {
         GumballMachine gumballMachine =
             new GumballMachine(10);
