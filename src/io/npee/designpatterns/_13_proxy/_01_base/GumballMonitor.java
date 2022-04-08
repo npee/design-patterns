1aca73aab7d9966676445f08f741541abd8dffb0
npee <jmc4664@gmail.com>
2022-04-08 21:50:40 +0900

    풍선껌 기계 모니터링 클래스 작성

---
+++ src/io/npee/designpatterns/_13_proxy/_01_base/GumballMonitor.java
@@ -0,0 +1,16 @@
+package io.npee.designpatterns._13_proxy._01_base;
+
+public class GumballMonitor {
+
+    GumballMachine gumballMachine;
+
+    public GumballMonitor(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void report() {
+        System.out.println("Gumball Machine: " + gumballMachine.getLocation());
+        System.out.println("Current inventory: " + gumballMachine.getCount() + " gumballs");
+        System.out.println("Current state: " + gumballMachine.getState());
+    }
+}
