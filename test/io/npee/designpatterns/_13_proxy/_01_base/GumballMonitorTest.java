7b224b699e6f03127a0bc970e19e34c1261a886a
npee <jmc4664@gmail.com>
2022-04-08 22:00:29 +0900

    풍선껌 기계 모니터링 클래스 테스트

---
+++ test/io/npee/designpatterns/_13_proxy/_01_base/GumballMonitorTest.java
@@ -0,0 +1,56 @@
+package io.npee.designpatterns._13_proxy._01_base;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GumballMonitorTest {
+
+    @Test
+    void gumballMonitorTest() {
+
+        GumballMachine gumballMachine = new GumballMachine(10, "GumballMachine");
+
+        GumballMonitor monitor = new GumballMonitor(gumballMachine);
+
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        monitor.report();
+    }
+}
