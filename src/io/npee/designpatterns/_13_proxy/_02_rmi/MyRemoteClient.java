76939a82bb4521af2a3c0ec64c5e12b92708a4f9
npee <jmc4664@gmail.com>
2022-04-10 20:49:28 +0900

    rmi 클라이언트 코드

---
+++ src/io/npee/designpatterns/_13_proxy/_02_rmi/MyRemoteClient.java
@@ -0,0 +1,23 @@
+package io.npee.designpatterns._13_proxy._02_rmi;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class MyRemoteClient {
+
+    public static void main(String[] args) {
+        new MyRemoteClient().go();
+    }
+
+    public void go() {
+        try {
+            MyRemote service = (MyRemote)Naming.lookup("rmi://127.0.0.1/RemoteHello");
+            String s = service.sayHello();
+            System.out.println(s);
+        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
